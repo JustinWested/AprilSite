aaa7eba733a6fd6f489b4e841db78b234239d994
JustinWested <justwested@gmail.com>
2023-04-22 23:18:43 -0700

    main home page complete. final image gallery added

--- js/main.js
+++ js/main.js
@@ -1,49 +1,85 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const imageThumbs = document.getElementById("image-thumbs");
+  const currentImage = document.getElementById("current-image");
+  const description = document.getElementById("description");
 
+  const descriptions = [
+      '<span>My newest short film, “Butt Stuff,” raised 124% of its original goal during its Seed&Spark crowdfunding campaign! You can still follow along by clicking here:<br><br><a href="https://seedandspark.com/fund/butt-stuff#story">Butt Stuff on Seed&Spark!</a></span>',
+      '<span>Catch me as the voice of Selkie the playful mermaid in the narrative podcast “Venice Magic Shop!”<br><br><a href="https://open.spotify.com/episode/5F4tMaIzsVWtgYy8D4L9pz?si=8bb2e037589544a1&nd=1">Now On Spotify!</a></span>',
+      '<span>I voice Blair in the new D&D series “Caves & Creatures.” <br><br><a href="https://www.youtube.com/watch?v=D_3In7YQP2U">Watch Season 1 Episode 1 Equal Opportunity Cannibal</a></span>',
+      '<span>My first film, “this is a garden,” is now on YouTube with 31,000 views and counting! You can watch it here:<br><br><a href="https://www.youtube.com/watch?v=b4eXILF8CIE">"this is a garden"</a></span>',
+      '<span>“Pulling the Plug on Mom” continues its festival run, most recently getting a nomination for Best Comedy at Cannes Shorts!<br><br><a href="#">Go to Page</a></span>',
+  ];
 
-// Get the hero and thumbnail images
-const heroImg = document.querySelector('.hero img');
-const thumbnailImgs = document.querySelectorAll('.thumbnail img');
+  for (let i = 1; i <= 5; i++) {
+      const thumb = document.createElement("img");
+      thumb.src = `images/indeximages/image${i}.webp`;
+      thumb.alt = `Image ${i}`;
+      thumb.dataset.description = descriptions[i - 1];
 
-// Set click listeners for thumbnail images
-thumbnailImgs.forEach((img) => {
-  img.addEventListener('click', () => {
-    heroImg.src = img.src;
-  });
-});
+      thumb.classList.add("thumb");
+      imageThumbs.appendChild(thumb);
 
-// Set click listener for hero image
-heroImg.addEventListener('click', () => {
-  // Get the index of the current image in the thumbnail images
-  let currentIndex;
-  for (let i = 0; i < thumbnailImgs.length; i++) {
-    if (thumbnailImgs[i].src === heroImg.src) {
-      currentIndex = i;
-      break;
-    }
+      thumb.addEventListener("click", function () {
+          currentImage.src = this.src;
+          currentImage.alt = this.alt;
+          description.innerHTML = this.dataset.description;
+          description.classList.remove("hidden");
+          description.style.height = `${currentImage.clientHeight}px`;
+      });
   }
-  // Get the next image in the thumbnail images
-  const nextIndex = (currentIndex + 1) % thumbnailImgs.length;
-  const nextImg = thumbnailImgs[nextIndex];
-  // Set the hero image to the next image
-  heroImg.src = nextImg.src;
-});
 
+  currentImage.addEventListener("load", () => {
+      description.style.height = `${currentImage.clientHeight}px`;
+  });
 
-const thumbnailsContainer = document.querySelector('.thumbnails-container');
-const thumbnails = document.querySelector('.thumbnails');
+  description.innerHTML = descriptions[0];
 
-thumbnailImgs.forEach((img) => {
-  img.addEventListener('click', () => {
-    const scrollWidth = thumbnails.scrollWidth - thumbnailsContainer.clientWidth;
-    const imgIndex = Array.from(thumbnailImgs).indexOf(img);
-    const imgWidth = img.offsetWidth;
-    const imgOffset = imgIndex * (imgWidth + 10);
-    thumbnailsContainer.scrollTo({
-      left: Math.min(imgOffset, scrollWidth),
-      behavior: 'smooth',
-    });
+  currentImage.addEventListener("mouseover", () => {
+      description.classList.remove("hidden");
+  });
+
+  currentImage.addEventListener("mouseout", () => {
+      description.classList.add("hidden");
+  });
+
+    function cycleImages() {
+      let currentIndex = 1;
+  
+      return setInterval(() => {
+          currentImage.classList.add("fade-out");
+  
+          setTimeout(() => {
+              currentIndex++;
+              if (currentIndex > 5) {
+                  currentIndex = 1;
+              }
+              const thumb = document.querySelector(`.thumb:nth-child(${currentIndex})`);
+              currentImage.src = thumb.src;
+              currentImage.alt = thumb.alt;
+              description.innerHTML = thumb.dataset.description;
+              description.style.height = `${currentImage.clientHeight}px`;
+  
+              currentImage.addEventListener("load", () => {
+                  currentImage.classList.remove("fade-out");
+                  currentImage.classList.add("fade-in");
+  
+                  setTimeout(() => {
+                      currentImage.classList.remove("fade-in");
+                  }, 300); // Match the duration of the fadeIn CSS
+              }, { once: true });
+          }, 300); // Match the duration of the fadeOut CSS
+      }, 5000); // Change the delay between image changes
+  }
+  
+
+  let cyclingInterval = cycleImages();
+
+  currentImage.addEventListener("mouseover", () => {
+      clearInterval(cyclingInterval);
+  });
+
+  currentImage.addEventListener("mouseout", () => {
+      cyclingInterval = cycleImages();
   });
 });
-
-
-
