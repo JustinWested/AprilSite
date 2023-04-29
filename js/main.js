5ec6c8cb51a0e4e6af9e23c4be06fcb24854257c
JustinWested <justwested@gmail.com>
2023-04-29 02:29:40 -0700

    pushing to test small mobile style changes

--- js/main.js
+++ js/main.js
@@ -308,3 +308,14 @@ document.addEventListener("DOMContentLoaded", () => {
     addLightboxListener(lightboxImage);
   });
 });
+
+/***************************************** */
+
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburger = document.querySelector('.hamburger');
+  const nav = document.querySelector('nav');
+
+  hamburger.addEventListener('click', () => {
+    nav.classList.toggle('show-menu');
+  });
+});
